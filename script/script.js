ae845415543ba1ff64e16ad1f964855e71e58fe5
jahidulkanchan <mdj524156@gmail.com>
2024-10-10 21:19:52 +0600

    update

--- script/script.js
+++ script/script.js
@@ -26,36 +26,28 @@ const displayAllFoods = (data) => {
         <div class="md:col-span-3">
           <h3 class="text-2xl font-bold">${item.strCategory}</h3>
           <p>${item.strCategoryDescription}</p>
-          <button class="text-xl font-semibold text-orange-300 underline">View Details</button>
+          <button class="text-xl view-btn font-semibold text-orange-300 underline">View Details</button>
         </div>
       `;
+      const detailsModal = document.getElementById('details-modal');
+      const viewBtn = document.getElementsByClassName('view-btn');
+      for(btn of viewBtn){
+      btn.addEventListener('click', ()=>{
+        detailsModal.classList.remove('hidden');
+      })
+    }
       foodContainer.appendChild(foodCart);
     });
+    
   }
   let sliceData = data.slice(0, 4);
   renderFoods(sliceData);
 };
 
-// const displayAllFoods = (data)=>{
-//   const foodContainer =  document.getElementById('food-container');
-//   const showAllItem =  document.getElementById('details-modal');
 
-//   let sliceData = data.slice(0, 2)
-//   showAllItem.addEventListener('click', ()=>{
-//       sliceData = data.slice(0, data.length);
-//       console.log('i am ok', data.length);
-//   })
-//   sliceData.forEach((item)=>{
-//     const foodCart = document.createElement('div');
-//     foodCart.classList.add('grid','grid-cols-5', 'gap-5', 'justify-between', 'items-center', 'border', 'py-5', 'rounded-lg')
-//     foodCart.innerHTML = `
-//     <div class="col-span-2"><img class="max-h-[200px]" src=${item.strCategoryThumb} alt=""></div>
-//     <div class="col-span-3">
-//       <h3 class="text-2xl font-bold">${item.strCategory}</h3>
-//       <p>${item.strCategoryDescription}</p>
-//       <button class="text-xl font-semibold text-orange-300 underline">View Details</button>
-//     `
-//     foodContainer.appendChild(foodCart)
-//   })
-// }
+const detailsModal = document.getElementById('details-modal');
+
+document.getElementById('close-btn').addEventListener('click', () =>{
+  detailsModal.classList.add('hidden')
+})
 getAllFoods()
